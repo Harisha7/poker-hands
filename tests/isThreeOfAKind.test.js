3e58488293ce6bae099c1b68f374a9c62cea4e07
harisha7 <harisha.srinivas74@gmail.com>
2023-04-19 13:21:28 +0200

    is three of a kind done

--- tests/isThreeOfAKind.test.js
+++ tests/isThreeOfAKind.test.js
@@ -2,7 +2,12 @@ const Hand = require('../Hand');
 const CompareHands = require('../CompareHands');
 const suits = '♥♦♣♠';
 
-test('Test that threeOfAKind returns truthy is three of a kind', () => {
+test('check that threeOfAKind returns truthy is three of a kind', () => {
   let hand = new Hand('♥7', '♦2', '♣7', '♠3', '♠7');
   expect(CompareHands.isThreeOfAKind(hand)).toBeTruthy();
+})
+
+test('check that threeOfAKind returns falsy is not three of a kind', () => {
+  let hand = new Hand('♥7', '♦2', '♣7', '♠3', '♠7');
+  expect(CompareHands.isThreeOfAKind(hand)).toBeFalsy();
 });
