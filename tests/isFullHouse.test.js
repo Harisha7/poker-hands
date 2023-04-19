629d77c98c73ae583f0f56d7dfcbd08d91b7c5ad
harisha7 <harisha.srinivas74@gmail.com>
2023-04-19 14:40:29 +0200

    Is full house test is passed.

--- tests/isFullHouse.test.js
+++ tests/isFullHouse.test.js
@@ -3,11 +3,11 @@ const CompareHands = require('../CompareHands');
 const suits = '♥♦♣♠';
 
 test('Check that FullHouse returns truthy is full house', () => {
-	let hand = new Hand('♥6','♦6','♣6','♠K','♣K');
-	except(CompareHands.isFullHouse(hand)).toBeTruthy();
-}) ;
+	let hand = new Hand('♠6','♥6','♦6','♣K','♥K' );
+	expect(CompareHands.isFullHouse(hand)).toBeTruthy();
+}) 
 
 test('Check that FullHouse returns falsy is not full house', () => {
-	let hand = new Hand('♥6','♦6','♣6','♠K','♣K');
-	except(CompareHands.isFullHouse(hand)).toBeFalsy();
+	let hand = new Hand('♥6','♦3','♣8','♠j','♣2');
+	expect(CompareHands.isFullHouse(hand)).toBeFalsy();
 }) ;
