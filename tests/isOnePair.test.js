64b28baef0c4060fcbbe994c87542e9c42198bf4
harisha7 <harisha.srinivas74@gmail.com>
2023-04-19 14:41:08 +0200

    Is one pair test is passed.

--- tests/isOnePair.test.js
+++ tests/isOnePair.test.js
@@ -3,11 +3,11 @@ const CompareHands = require('../CompareHands');
 const suits = '♥♦♣♠';
 
 test('Check that isOnePair returns truthy is one pair', () => {
-	let hand = new Hand('♥7','♥10','♣4','♠8','10♠');
-	except(CompareHands.isOnePair(hand)).toBeTruthy();
+	let hand = new Hand('♠10','♥10','♠8','♥7','♣4');
+	expect(CompareHands.isOnePair(hand)).toBeTruthy();
 })
 
 test('Check that isOnePair returns falsy is not one pair', () => {
-	let hand = new Hand('♥7','♦7','♣7','♠7','4♠');
-	except(CompareHands.isOnePair(hand)).toBeFalsy();
+	let hand = new Hand('♥k','♦2','♣4','♠5','6♠');
+	expect(CompareHands.isOnePair(hand)).toBeFalsy();
 });
