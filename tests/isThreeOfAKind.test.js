4f30146461d6928ba9dbe6c049421a5cef38e0fa
harisha7 <harisha.srinivas74@gmail.com>
2023-04-19 14:42:20 +0200

    Is three of a kind test passed.

--- tests/isThreeOfAKind.test.js
+++ tests/isThreeOfAKind.test.js
@@ -8,6 +8,6 @@ test('check that threeOfAKind returns truthy is three of a kind', () => {
 })
 
 test('check that threeOfAKind returns falsy is not three of a kind', () => {
-  let hand = new Hand('♥7', '♦2', '♣7', '♠3', '♠7');
+  let hand = new Hand('♥6', '♦2', '♣5', '♠3', '♠7');
   expect(CompareHands.isThreeOfAKind(hand)).toBeFalsy();
 });
