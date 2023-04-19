bd4cad60756f7439fe9eedce331632342f5dd313
harisha7 <harisha.srinivas74@gmail.com>
2023-04-19 13:20:55 +0200

    is four of a kind done

--- tests/isFourOfAKind.test.js
+++ tests/isFourOfAKind.test.js
@@ -2,7 +2,13 @@ const Hand = require('../Hand');
 const CompareHands = require('../CompareHands');
 const suits = '♥♦♣♠';
 
-test('Test that FourOfAKind returns truthy is four of a kind', () => {
+test('Check that FourOfAKind returns truthy is four of a kind', () => {
 	let hand = new Hand('♥7','♦7','♣7','♠7','4♠');
 	except(CompareHands.isFourOfAKind(hand)).toBeTruthy();
+})
+
+
+test('Check that FourOfAKind returns falsy is not four of a kind', () => {
+	let hand = new Hand('♥7','♦7','♣7','♠7','4♠');
+	except(CompareHands.isFourOfAKind(hand)).toBeFalsy();
 });
