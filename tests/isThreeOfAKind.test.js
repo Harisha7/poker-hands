3188bc6613eca1dce3fd8092c57c2aea4d55d684
harisha7 <harisha.srinivas74@gmail.com>
2023-04-26 15:01:48 +0200

    is three of a kind test done

--- tests/isThreeOfAKind.test.js
+++ tests/isThreeOfAKind.test.js
@@ -11,3 +11,17 @@ test('check that threeOfAKind returns falsy is not three of a kind', () => {
   let hand = new Hand('♥6', '♦2', '♣5', '♠3', '♠7');
   expect(CompareHands.isThreeOfAKind(hand)).toBeFalsy();
 });
+
+
+test('check that falsy threeOfAKind returns as 0', () => {
+  let hand = new Hand('♥6', '♦2', '♣5', '♠3', '♠7');
+  expect(CompareHands.isThreeOfAKind(hand)).toBe(0);
+});
+
+test('test that isThreeOfAKind returns a higher score for a stronger hand (if two hands but with three of a kind)', () => {
+  let hand1 = new Hand('♥7', '♦2', '♣7', '♠3', '♠7');
+  let hand2 = new Hand('♥6', '♦2', '♣5', '♠3', '♠7');
+  let hand1Score = CompareHands.isThreeOfAKind(hand1);
+  let hand2Score = CompareHands.isThreeOfAKind(hand2);
+  expect(hand1Score).toBeGreaterThan(hand2Score);
+});
