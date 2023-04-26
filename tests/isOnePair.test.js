160d221f3c3dabf72e4cfc94872fcf47fbf7ebe4
harisha7 <harisha.srinivas74@gmail.com>
2023-04-26 14:56:37 +0200

    is one pair test done

--- tests/isOnePair.test.js
+++ tests/isOnePair.test.js
@@ -11,3 +11,16 @@ test('Check that isOnePair returns falsy is not one pair', () => {
 	let hand = new Hand('♥k','♦2','♣4','♠5','6♠');
 	expect(CompareHands.isOnePair(hand)).toBeFalsy();
 });
+
+test('Check that falsy isOnePair returns as 0', () => {
+	let hand = new Hand('♥k','♦2','♣4','♠5','6♠');
+	expect(CompareHands.isOnePair(hand)).toBe(0);
+});
+
+test('test that isOnePair returns a higher score for a stronger hand (if two hands but with one pair)', () => {
+  let hand1 = new Hand('♠10','♥10','♠8','♥7','♣4');
+  let hand2 = new Hand('♥k','♦2','♣4','♠5','6♠');
+  let hand1Score = CompareHands.isOnePair(hand1);
+  let hand2Score = CompareHands.isOnePair(hand2);
+  expect(hand1Score).toBeGreaterThan(hand2Score);
+});
