6df00586501b0069da48454a1bd1e4170aaacb6e
harisha7 <harisha.srinivas74@gmail.com>
2023-04-26 14:55:52 +0200

    is flush test done

--- tests/isFlush.test.js
+++ tests/isFlush.test.js
@@ -2,19 +2,24 @@ const Hand = require('../Hand');
 const CompareHands = require('../CompareHands');
 const suits = '♥♦♣♠';
 
-test('check that isFlush returns truthy if flush', () => {
+test('test that isFlush returns truthy if flush', () => {
   for (let suit of suits) {
     let hand = new Hand(suit + '2', suit + '6', suit + '4', suit + '8', suit + '7');
     expect(CompareHands.isFlush(hand)).toBeTruthy();
   }
 });
 
-test('check that isFlush returns falsey if not flush', () => {
+test('test that isFlush returns falsey if not flush', () => {
   let hand = new Hand('♣2', '♣6', '♥4', '♣8', '♣7');
   expect(CompareHands.isFlush(hand)).toBeFalsy();
 });
 
-test('check that isFlush returns a higher score for a stronger hand (if two hands but with flush)', () => {
+test('test that falsy flush returns as 0', () =>{
+  let hand = new Hand('♣2', '♣6', '♥4', '♣8', '♣7');
+  expect(CompareHands.isFlush(hand)).toBe(0)
+});
+
+test('test that isFlush returns a higher score for a stronger hand (if two hands but with flush)', () => {
   let hand1 = new Hand('♣2', '♣6', '♣4', '♣8', '♣7');
   let hand2 = new Hand('♦T', '♦9', '♦5', '♦Q', '♦A');
   let hand1Score = CompareHands.isFlush(hand1);
