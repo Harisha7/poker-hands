c29ca70571896448ba77094c1b3155324b70fb6d
harisha7 <harisha.srinivas74@gmail.com>
2023-04-26 14:56:07 +0200

    is four of a kind test done

--- tests/isFourOfAKind.test.js
+++ tests/isFourOfAKind.test.js
@@ -12,3 +12,16 @@ test('Check that is FourOfAKind returns falsy is not four of a kind', () => {
 	let hand = new Hand('♣5','♦5','♣6','♥4','♣7');
 	expect(CompareHands.isFourOfAKind(hand)).toBeFalsy();
 });
+
+test('Check that falsy FourOfAKind returns as 0', () => {
+	let hand = new Hand('♣5','♦5','♣6','♥4','♣7');
+	expect(CompareHands.isFourOfAKind(hand)).toBe(0);
+});
+
+test('test that isFourOfAKind returns a higher score for a stronger hand (if two hands but with four of a kind)', () => {
+  let hand1 = new Hand('♥4','♦4','♣4','♠4','♠7');
+  let hand2 = new Hand('♣5','♦5','♣6','♥4','♣7');
+  let hand1Score = CompareHands.isFourOfAKind(hand1);
+  let hand2Score = CompareHands.isFourOfAKind(hand2);
+  expect(hand1Score).toBeGreaterThan(hand2Score);
+});
