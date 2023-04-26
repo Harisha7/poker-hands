58a45552175a4cb31b0399397477e50f741e04cf
harisha7 <harisha.srinivas74@gmail.com>
2023-04-26 14:55:23 +0200

    Check duplicate code is added

--- CompareHands.js
+++ CompareHands.js
@@ -1,6 +1,7 @@
 module.exports = class CompareHands {
 
   static ranks = '23456789TJQKA';
+  static suits = '♠♥♦♣'
 
   // Helper methods (used by the comparer methods)
 
@@ -137,5 +138,72 @@ module.exports = class CompareHands {
   static isHighestCard(hand) {
     return this.scoreWithKickers(0, hand.cards);
   }
+
+  static getArray(array) {
+    var a = array.concat();
+    for (var i = 0; i < a.length; ++i) {
+      for (var j = i + 1; j < a.length; ++j) {
+        if ((a[i].suit + a[i].rank) === (a[j].suit + a[j].rank))
+          a.splice(j--, 1);
+      }
+    }
+
+    return a;
+  }
+
+  static checkIfNotDuplicate(hand1, hand2) {
+    let allCards = hand1.cards
+    if (hand2 != null) {
+      allCards = this.getArray(allCards.concat(hand2.cards))
+    }
+    else{
+      allCards = this.getArray(allCards)
+    }
+    if ((hand2 == null) && (allCards.length == hand1.cards.length)) {
+      return true
+    }
+    else if ((hand2 != null) && (allCards.length == (hand1.cards.length + hand2.cards.length))) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+ /*static getUniqueArray(array) {
+    var a = array.concat();
+    for (var i = 0; i < a.length; ++i) {
+      for (var j = i + 1; j < a.length; ++j) {
+        if ((a[i].suit + a[i].rank) === (a[j].suit + a[j].rank))
+          a.splice(j--, 1);
+      }
+    }
+
+    return a;
+  }
+  static checkIfNotContainDouble(hand1, hand2) {
+    let allCards = hand1.cards
+    if (hand2 != null) {
+      allCards = this.getUniqueArray(allCards.concat(hand2.cards))
+
+    }
+    else {
+      allCards = this.getUniqueArray(allCards)
+    }
+    if ((hand2 == null) && (allCards.length == hand1.cards.length)) {
+      return true
+    }
+    else if ((hand2 != null) && (allCards.length == (hand1.cards.length + hand2.cards.length))) {
+      return true
+    }
+
+    else {
+      return false
+    }
+
+  }*/
+
+
+  
 
 }
