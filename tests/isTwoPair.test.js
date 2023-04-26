4cd819f3dd3ad8d26df30f7c4fa6baf20e0bd940
harisha7 <harisha.srinivas74@gmail.com>
2023-04-26 15:01:59 +0200

    is two pair test done

--- tests/isTwoPair.test.js
+++ tests/isTwoPair.test.js
@@ -11,3 +11,16 @@ test('Check that isTwoPair returns falsy is not two pair', () => {
 	let hand = new Hand('♥7','♦3','♣2','♠6','5♠');
 	expect(CompareHands.isTwoPair(hand)).toBeFalsy();
 });
+
+test('Check that falsy isTwoPair returns as 0', () => {
+	let hand = new Hand('♥7','♦3','♣2','♠6','5♠');
+	expect(CompareHands.isTwoPair(hand)).toBe(0);
+});
+
+test('test that isTwoPair returns a higher score for a stronger hand (if two hands but with flush)', () => {
+  let hand1 = new Hand('♥J','♠J','♣3','♠3','♥2');
+  let hand2 = new Hand('♥7','♦3','♣2','♠6','5♠');
+  let hand1Score = CompareHands.isTwoPair(hand1);
+  let hand2Score = CompareHands.isTwoPair(hand2);
+  expect(hand1Score).toBeGreaterThan(hand2Score);
+});
