5ab3f8e0688cd4c3c50781e28a0167114ff23ad3
harisha7 <harisha.srinivas74@gmail.com>
2023-04-26 15:00:51 +0200

    is straight flush test done

--- tests/isStraightFlush.test.js
+++ tests/isStraightFlush.test.js
@@ -11,3 +11,16 @@ test('Check that isStraightFlush returns falsy is not straight flush', () => {
 	let hand = new Hand('♥3','♦4','♣6','♠2','♦A');
 	expect(CompareHands.isStraightFlush(hand)).toBeFalsy();
 }) ;
+
+test('Check that falsy isStraightFlush returns as 0', () => {
+	let hand = new Hand('♥3','♦4','♣6','♠2','♦A');
+	expect(CompareHands.isStraightFlush(hand)).toBe(0);
+}) ;
+
+test('test that isStraightFlush returns a higher score for a stronger hand (if two hands but with flush)', () => {
+  let hand1 = new Hand('♥3','♦4','♣6','♠2','♦A');
+  let hand2 = new Hand('♥A','♥K','♥Q','♥J','♥T');
+  let hand1Score = CompareHands.isStraightFlush(hand1);
+  let hand2Score = CompareHands.isStraightFlush(hand2);
+  expect(hand2Score).toBeGreaterThan(hand1Score);
+});
